f395fe751ec6ff65706409914ab7b9d673c304be
ting-prado <ethel.prado1998@gmail.com>
2022-08-31 10:12:11 +0800

    transferred several components directly into the form

--- src/components/FormParts.js
+++ src/components/FormParts.js
@@ -2,25 +2,12 @@ import { Component } from 'react';
 import '../styles/form.css';
 
 export class ReverseField extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      input: '',
-    };
-
-    this.handleInput = this.handleInput.bind(this);
-  }
-  handleInput = (e) => {
-    console.log(e.target);
-  };
-
   render() {
-    const { labelName, name } = this.props;
+    const { handleChange, labelName, name } = this.props;
     return (
       <div className="reverseField">
         <input
-          onClick={this.handleInput}
+          onChange={handleChange}
           type="text"
           name={name}
           placeholder={labelName}
@@ -33,48 +20,16 @@ export class ReverseField extends Component {
 
 export class Field extends Component {
   render() {
-    const { labelName, name } = this.props;
+    const { handleChange, labelName, name } = this.props;
     return (
       <div className="field">
         <label>{labelName}</label>
-        <input type="text" name={name} placeholder={labelName} />
-      </div>
-    );
-  }
-}
-
-export class NameInput extends Component {
-  render() {
-    return (
-      <fieldset className="fullName">
-        <legend>Full Name</legend>
-        <ReverseField name="firstName" labelName="First Name" />
-        <ReverseField name="lastName" labelName="Last Name" />
-        <ReverseField name="midInitial" labelName="Middle Initial" />
-      </fieldset>
-    );
-  }
-}
-
-export class ContactInput extends Component {
-  render() {
-    return (
-      <fieldset className="contact">
-        <legend>Contact Information</legend>
-        <ReverseField name="email" labelName="Email Address" />
-        <ReverseField name="contactNum" labelName="Contact Number" />
-      </fieldset>
-    );
-  }
-}
-
-export class Desc extends Component {
-  render() {
-    const { labelName, name } = this.props;
-    return (
-      <div className="desc">
-        <label>{labelName}</label>
-        <textarea rows="5" name={name} placeholder={labelName}></textarea>
+        <input
+          onChange={handleChange}
+          type="text"
+          name={name}
+          placeholder={labelName}
+        />
       </div>
     );
   }
